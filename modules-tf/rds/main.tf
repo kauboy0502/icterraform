ec63fb00b1b1ec63456823a4d61eeb4f7bf4f2b8
kauboy0502 <88572669+kauboy0502@users.noreply.github.com>
2023-01-06 16:22:22 +0530

    Add files via upload

--- modules-tf/rds/main.tf
+++ modules-tf/rds/main.tf
@@ -1,1 +1,25 @@
+resource "aws_db_instance" "default" {
+    
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "postgresgroup"
+  skip_final_snapshot  = true
+  vpc_security_group_ids = [var.sgs]
+  db_subnet_group_name = aws_db_subnet_group.dbsubnetgroup.name
+}
 
+resource "aws_db_subnet_group" "dbsubnetgroup" {
+    
+  name       = "dbsubgroup"
+  subnet_ids = var.dbsubgrp
+
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
---
+++ modules-tf/rds/variables.tf
@@ -0,0 +1,18 @@
+variable dbsubgrp {
+    type = list(string)
+
+}
+
+variable "sgs" {
+    
+  
+}
+
+variable engine {
+    default = "postgres"
+}
+
+variable "engine_version" {
+    default = "10.14"
+  
+}
